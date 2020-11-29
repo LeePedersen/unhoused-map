66c0c9656d71f98f3d29c5111083d4d1678611f3
Lee Pedersen <leepedersenr@gmail.com>
2020-11-29 12:42:50 -0700

    fix clearMarkers function

--- js/scripts.js
+++ js/scripts.js
@@ -5,31 +5,31 @@ const tent = "./tentIconResized.png";
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 39.75466197035792, lng: -104.98515233629519 },
-    zoom: 13.5,
+    zoom: 14,
     styles: mapStyle
   });
-  addMarkers();
+  addMonths();
 }
 
-function addMarkers() {
-  // iterate through the months, 2 second wait time on first month, 4 second on second one and so on
-  Object.entries(campsByMonth).forEach((month, key) =>
-    addMarkersWithTimeout(month[1], (2000 + (key * 2000)))
-  );
-  // iterate through the camps in the month, add them all
-  function addMarkersWithTimeout(month, timeout) {
-    Object.entries(month).forEach((camp, key) => {
-      window.setTimeout(() => {
-        markers.push(
-          new google.maps.Marker({
-            position: camp[1].center,
-            map,
-            // icon: tent
-          })
-        );
-      }, timeout)
-    })
-  }
+function addMonths() {
+  Object.entries(campsByMonth).forEach((month, key) => {
+    window.setTimeout(() => {
+      clearMarkers();
+      addMarkers(month[1]);
+    }, (2000 + (key * 2000)));
+  });
+}
+
+function addMarkers(month) {
+  Object.entries(month).forEach((camp, key) => {
+    markers.push(
+      new google.maps.Marker({
+        position: camp[1].center,
+        map,
+        // icon: tent
+      })
+    );
+  })
 }
 
 function clearMarkers() {
