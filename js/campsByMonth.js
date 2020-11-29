f625487098b427b9fe275127090330b056a29c87
Lee Pedersen <leepedersenr@gmail.com>
2020-11-29 13:19:49 -0700

    more data entry

--- js/campsByMonth.js
+++ js/campsByMonth.js
@@ -75,4 +75,208 @@ const campsByMonth = {
       population: 50,
     },
   },
+  juneCamps: {
+    stout22nd: {
+      center: { lat: 39.7514, lng: -104.9858 },
+      population: 150,
+    },
+    clarkson13th: {
+      center: { lat: 39.73688467944993, lng: -104.97760917348667 },
+      population: 100,
+    },
+    lincolnPark: {
+      center: { lat: 39.7320785592142, lng: -105.00244677021708 },
+      population: 100,
+    },
+    arkins: {
+      center: { lat: 39.770366437456126, lng: -104.98156603283451 },
+      population: 150,
+    },
+    welton29th: {
+      center: { lat: 39.75667593136853, lng: -104.97563640226254 },
+      population: 4,
+    },
+    arapahoe25th: {
+      center: { lat: 39.7563385156032, lng: -104.98483701760286 },
+      population: 50,
+    },
+    welton25th: {
+      center: { lat: 39.75298262156344, lng: -104.98049675993238 },
+      population: 20,
+    },
+    blake20th: {
+      center: { lat: 39.753924220898696, lng: -104.99384281575655 },
+      population: 20,
+    },
+    cherokeeSantaFe: {
+      center: { lat: 39.70408514136941, lng: -104.99157753535594 },
+      population: 20,
+    },
+    blairCaldwell: {
+      center: { lat: 39.75240922793768, lng: -104.98160900226182 },
+      population: 50,
+    },
+  },
+  julyCamps: {
+    lincolnPark: {
+      center: { lat: 39.7320785592142, lng: -105.00244677021708 },
+      population: 100,
+    },
+    clarkson13th: {
+      center: { lat: 39.73688467944993, lng: -104.97760917348667 },
+      population: 100,
+    },
+    stout22nd: {
+      center: { lat: 39.7514, lng: -104.9858 },
+      population: 150,
+    },
+    arkins: {
+      center: { lat: 39.770366437456126, lng: -104.98156603283451 },
+      population: 150,
+    },
+    welton29th: {
+      center: { lat: 39.75667593136853, lng: -104.97563640226254 },
+      population: 4,
+    },
+    arapahoe25th: {
+      center: { lat: 39.7563385156032, lng: -104.98483701760286 },
+      population: 50,
+    },
+    blake20th: {
+      center: { lat: 39.753924220898696, lng: -104.99384281575655 },
+      population: 20,
+    },
+    welton25th: {
+      center: { lat: 39.75298262156344, lng: -104.98049675993238 },
+      population: 20,
+    },
+    cherokeeSantaFe: {
+      center: { lat: 39.70408514136941, lng: -104.99157753535594 },
+      population: 20,
+    },
+    broadway19th: {
+      center: { lat: 39.746235466647214, lng: -104.98744545088414 },
+      population: 20,
+    },
+    blairCaldwell: {
+      center: { lat: 39.75240922793768, lng: -104.98160900226182 },
+      population: 50,
+    },
+  },
+  augustCamps: {
+    clarkson13th: {
+      center: { lat: 39.73688467944993, lng: -104.97760917348667 },
+      population: 100,
+    },
+    acoma12th: {
+      center: { lat: 39.735250472810364, lng: -104.98888505115747 },
+      population: 100,
+    },
+    washington13th: {
+      center: { lat: 39.736901630492326, lng: -104.97873995993287 },
+      population: 100,
+    },
+    welton29th: {
+      center: { lat: 39.75667593136853, lng: -104.97563640226254 },
+      population: 4,
+    },
+    stout22nd: {
+      center: { lat: 39.7514, lng: -104.9858 },
+      population: 150,
+    },
+    arkins: {
+      center: { lat: 39.770366437456126, lng: -104.98156603283451 },
+      population: 150,
+    },
+    logan14th: {
+      center: { lat: 39.73840227214621, lng: -104.98232513581428 },
+      population: 50,
+    },
+    pennsylvania14th: {
+      center: { lat: 39.73843667280359, lng: -104.9811901664974 },
+      population: 50,
+    },
+    lincolnPark: {
+      center: { lat: 39.7320785592142, lng: -105.00244677021708 },
+      population: 100,
+    },
+    arapahoe25th: {
+      center: { lat: 39.7563385156032, lng: -104.98483701760286 },
+      population: 50,
+    },
+    blake20th: {
+      center: { lat: 39.753924220898696, lng: -104.99384281575655 },
+      population: 20,
+    },
+    welton25th: {
+      center: { lat: 39.75298262156344, lng: -104.98049675993238 },
+      population: 20,
+    },
+    broadway19th: {
+      center: { lat: 39.746235466647214, lng: -104.98744545088414 },
+      population: 20,
+    },
+    cherokeeSantaFe: {
+      center: { lat: 39.70408514136941, lng: -104.99157753535594 },
+      population: 20,
+    },
+    blairCaldwell: {
+      center: { lat: 39.75240922793768, lng: -104.98160900226182 },
+      population: 50,
+    },
+  },
+  septemberCamps: {
+    clarkson13th: {
+      center: { lat: 39.73688467944993, lng: -104.97760917348667 },
+      population: 100,
+    },
+    acoma12th: {
+      center: { lat: 39.735250472810364, lng: -104.98888505115747 },
+      population: 100,
+    },
+    washington13th: {
+      center: { lat: 39.736901630492326, lng: -104.97873995993287 },
+      population: 100,
+    },
+    welton29th: {
+      center: { lat: 39.75667593136853, lng: -104.97563640226254 },
+      population: 4,
+    },
+    stout22nd: {
+      center: { lat: 39.7514, lng: -104.9858 },
+      population: 150,
+    },
+    arkins: {
+      center: { lat: 39.770366437456126, lng: -104.98156603283451 },
+      population: 150,
+    },
+    logan14th: {
+      center: { lat: 39.73840227214621, lng: -104.98232513581428 },
+      population: 50,
+    },
+    pennsylvania14th: {
+      center: { lat: 39.73843667280359, lng: -104.9811901664974 },
+      population: 30,
+    },
+    broadway19th: {
+      center: { lat: 39.746235466647214, lng: -104.98744545088414 },
+      population: 20,
+    },
+    cherokeeSantaFe: {
+      center: { lat: 39.70408514136941, lng: -104.99157753535594 },
+      population: 20,
+    },
+    blairCaldwell: {
+      center: { lat: 39.75240922793768, lng: -104.98160900226182 },
+      population: 50,
+    },
+    wazee22nd: {
+      center: { lat: 39.756832830282285, lng: -104.99282652046314 },
+      population: 60,
+    },
+    sherman6th: {
+      center: { lat: 39.7256620796036, lng: -104.9848469204793 },
+      population: 30,
+    },
+  },
 };
